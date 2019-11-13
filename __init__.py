8796457ce2af1432d87ab3f6ef73862514958660
nvegakrutzfeldt <54637989+nvegakrutzfeldt@users.noreply.github.com>
2019-11-12 18:18:57 -0800

    New Features
    Added by hand not with terminal

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+login = LoginManager(app)
+# right side is the function that's called to login users
+login.login_view = 'login'
+
+from myapp import routes, models
---
+++ form.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, ValidationError
+
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name')
+    password = PasswordField('Password')
+    login = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('User Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
